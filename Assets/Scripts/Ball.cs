3511d04a68b0f0d5033e34f375aa66475fff9a28
javier ramos <javier ramos>
2017-03-26 11:15:56 -0600

    Mecanica de juego

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	public GameObject player;
+
+	private Component script;
+	private Rigidbody2D rb;
+	private Vector2 sum;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+		script = player.GetComponent<MovementPlayer> ();
+		rb = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+
+		if (other.gameObject.name.Equals ("down")) {
+			Debug.Log ("hit");
+			Destroy (this.gameObject);
+			MovementPlayer.state = false;
+		} else if (other.gameObject.tag.Equals ("Wall")) {
+			Destroy (other.gameObject);
+		} 
+	}
+}
---
+++ Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float v;
+	public static bool state;
+	public GameObject ball;
+
+	private Rigidbody2D rbb;
+	private GameObject ballprefab;
+
+	private float x;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		v = v * 10;
+		state = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (state) {
+			Move ();
+		} else {
+			Shoot ();
+		}
+	}
+
+	void Move(){
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			x = v;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			x = -v;
+		} else {
+			x = 0;
+		}
+		rb.AddForce (new Vector2 (x,0));
+	}
+
+	void Shoot(){
+		rb.transform.position = new Vector2 (0, -4f);
+		if (ballprefab == null) {
+			ballprefab = Instantiate (ball, rb.transform.position, Quaternion.identity);
+			rbb = ballprefab.GetComponent<Rigidbody2D> ();	
+		} else {
+			if (Input.GetKey (KeyCode.Space)){
+				rbb.velocity = new Vector2 (0, 5f);
+				ChangeState();
+			}
+		}
+
+	}
+
+	public void ChangeState(){
+		state = !state;
+	}
+
+
+}
